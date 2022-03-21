211ec65e44340c07abed7f893423b0a62eddfe8f
Arnab Pal <39365010+palarnab@users.noreply.github.com>
2022-03-21 18:40:01 +0530

    initial code added
    Also exists here: https://www.codeproject.com/Articles/134475/Method-for-Edge-Detection-in-Color-Images-Using-1

---
+++ BitmapAsTexture.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace MyBitmapedTextures
+{
+    class Pixel
+    {
+        public int Position {get; set;}
+        public double Intensity {get; set;}
+
+        public Pixel(double intensity, int position)
+        {
+            Position = position;
+            Intensity = intensity;
+        }
+    }
+
+    class BitmapAsTexture
+    {
+        private FastBitmap map;
+        private Algorithm algo;
+
+        public BitmapAsTexture(Bitmap bmp)
+        {
+            map = new FastBitmap(bmp);
+            map.LockBitmap();
+
+            algo = new Algorithm();
+        }
+
+        public Bitmap Get1DTexture(Point start, Point end)
+        {
+            int width = algo.GetDistance(start, end);
+
+            IList<Point> points = algo.GenerateLinePoints(start, end);
+
+            FastBitmap oneD = new FastBitmap(width, 1);
+            oneD.LockBitmap();
+
+            int max = points.Count < width ? points.Count : width;
+            int lastPixel = 0;
+
+            for (int i = 0; i < max; i++)
+            {
+                Point pt = points[i];
+                if (max == width)
+                {
+                    oneD.SetPixel(i, 0, map.GetPixel(pt.X, pt.Y));
+                }
+                else
+                {
+                    int x1D = Convert.ToInt32((float)i / points.Count * width);
+                    PixelData pixel = map.GetPixel(pt.X, pt.Y);
+                    oneD.SetPixel(x1D, 0, pixel);
+
+                    for (int j = x1D - 1; j > lastPixel; j--)
+                    {
+                        oneD.SetPixel(j, 0, pixel);
+                    }
+
+                    lastPixel = x1D;
+                }
+            }
+
+            oneD.UnlockBitmap();
+            return oneD.Bitmap;
+        }
+
+        public Bitmap ShowEdgePoints(Bitmap texture1D, int maxEdges, ColorSubtracter subtype, int tolerance, Color color)
+        {
+            ICustomCollection collection = null;
+
+            if (subtype is LuminoSubtractor)
+            {
+                collection = new IntensityStack(maxEdges);
+            }
+            else
+            {
+                collection = new BoundedSortedCollection(maxEdges);
+            }
+
+            return SetEdgePoints(texture1D, collection, subtype, tolerance, color); ;
+        }
+
+        private Bitmap SetEdgePoints(Bitmap tex, ICustomCollection collection, ColorSubtracter subtype, int tolerance, Color color)
+        {
+            Bitmap blankTex1D = new Bitmap(tex.Width, tex.Height);
+
+            Graphics g = Graphics.FromImage(blankTex1D);
+            g.Clear(Color.FromArgb(255 - color.R, 255 - color.G, 255 - color.B));
+            g.Dispose();
+
+            FastBitmap basetex = new FastBitmap(tex);
+            FastBitmap edgetex = new FastBitmap(blankTex1D);
+
+            basetex.LockBitmap();
+            edgetex.LockBitmap();
+
+            double lastDiff = 0.0;
+
+            for (int i = 0; i < tex.Width - 1; i++)
+            {
+                Color c1 = basetex.GetPixel(i, 0).ToColor();
+                Color c2 = basetex.GetPixel(i + 1, 0).ToColor();
+
+                double diff = subtype.GetColorDiff(c1, c2);
+                double absDiff = Math.Abs(diff);
+                double stepDiff = Math.Abs(absDiff - lastDiff);
+
+                if (absDiff > tolerance && stepDiff > tolerance)
+                {
+                    collection.Add(new Pixel(diff, i));
+                }
+
+                lastDiff = absDiff;
+            }
+
+            collection.Block();
+
+            for (int i = 0; i < collection.Count; i++)
+            {
+                int pos = collection.GetItem(i).Position;
+                edgetex.SetPixel(pos, 0, new PixelData(color));
+            }
+
+            edgetex.UnlockBitmap();
+            basetex.UnlockBitmap();
+
+            return edgetex.Bitmap;
+        }
+
+        public void Dispose()
+        {
+            map.UnlockBitmap();
+        }
+    }
+}
---
+++ ScanStrategy.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace MyBitmapedTextures
+{
+    struct PointPair
+    {
+        public Point Start;
+        public Point End;
+
+        public PointPair(Point start, Point end)
+        {
+            Start = start;
+            End = end;
+        }
+    }
+
+    public interface ScanStrategy
+    {
+        double Scan(int width, int height, int maxedges, int tolerance);
+        ColorSubtracter Strategy { get; set; }
+    }
+
+    class HScan : ScanStrategy
+    {
+        private BitmapAsTexture bmptex;
+        private Graphics graphics;
+        private Color edgecolor;
+        private object syncObj = new object();
+
+        public ColorSubtracter Strategy { get; set; }
+
+        public HScan(BitmapAsTexture b, Graphics g, Color e, ColorSubtracter c)
+        {
+            lock (syncObj)
+            {
+                bmptex = b;
+                graphics = g;
+                edgecolor = e;
+                Strategy = c;
+            }
+        }
+
+        public double Scan(int width, int height, int maxedges, int tolerance)
+        {
+            lock (syncObj)
+            {
+                DateTime start = DateTime.Now;
+
+                foreach (PointPair pointpair in GetPoints(width, height))
+                {
+                    Bitmap tex1d = null, onlyedges = null;
+
+                    try
+                    {
+                        tex1d = bmptex.Get1DTexture(pointpair.Start, pointpair.End);
+                        onlyedges = bmptex.ShowEdgePoints(tex1d, maxedges, Strategy, tolerance, edgecolor);
+                        graphics.DrawImage(onlyedges, pointpair.Start);
+                    }
+                    finally
+                    {
+                        if (tex1d != null)
+                        {
+                            tex1d.Dispose();
+                        }
+                        if (onlyedges != null)
+                        {
+                            onlyedges.Dispose();
+                        }
+                    }
+                }
+
+                return (DateTime.Now - start).TotalMilliseconds;
+            }
+        }
+
+        private IList<PointPair> GetPoints(int width, int height)
+        {
+            IList<PointPair> result = new List<PointPair>();
+
+            for (int i = 0; i < height; i++)
+            {
+                result.Add(new PointPair(new Point(0, i), new Point(width, i)));
+            }
+
+            return result;
+        }
+    }
+
+    class VScan : ScanStrategy
+    {
+        private BitmapAsTexture bmptex;
+        private Graphics graphics;
+        private Color edgecolor;
+        private object syncObj = new object();
+
+        public ColorSubtracter Strategy { get; set; }
+
+        public VScan(BitmapAsTexture b, Graphics g, Color e, ColorSubtracter c)
+        {
+            lock (syncObj)
+            {
+                bmptex = b;
+                graphics = g;
+                edgecolor = e;
+                Strategy = c;
+            }
+        }
+
+        public double Scan(int width, int height, int maxedges, int tolerance)
+        {
+            lock (syncObj)
+            {
+                DateTime start = DateTime.Now;
+
+                foreach (PointPair pointpair in GetPoints(width, height))
+                {
+                    Bitmap tex1d = null, onlyedges = null;
+
+                    try
+                    {
+                        tex1d = bmptex.Get1DTexture(pointpair.Start, pointpair.End);
+                        onlyedges = bmptex.ShowEdgePoints(tex1d, maxedges, Strategy, tolerance, edgecolor);
+                        onlyedges.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        graphics.DrawImage(onlyedges, pointpair.Start);
+                    }
+                    finally
+                    {
+                        if (tex1d != null)
+                        {
+                            tex1d.Dispose();
+                        }
+                        if (onlyedges != null)
+                        {
+                            onlyedges.Dispose();
+                        }
+                    }
+                }
+
+                return (DateTime.Now - start).TotalMilliseconds;
+            }
+        }
+
+        private IList<PointPair> GetPoints(int width, int height)
+        {
+            IList<PointPair> result = new List<PointPair>();
+
+            for (int i = 0; i < width; i++)
+            {
+                result.Add(new PointPair(new Point(i, 0), new Point(i, height)));
+            }
+
+            return result;
+        }
+    }
+
+    class InclinedScan : ScanStrategy
+    {
+        private BitmapAsTexture bmptex;
+        private Graphics graphics;
+        private Color edgecolor;
+        private int angle;
+        private object syncObj = new object();
+        private const double DEGREE_TO_RADIAN = 0.017453292519943295769236907684883;
+
+        public ColorSubtracter Strategy { get; set; }
+
+        public InclinedScan(int a, BitmapAsTexture b, Graphics g, Color e, ColorSubtracter c)
+        {
+            lock (syncObj)
+            {
+                angle = a;
+                bmptex = b;
+                graphics = g;
+                edgecolor = e;
+                Strategy = c;
+            }
+        }
+
+        public double Scan(int width, int height, int maxedges, int tolerance)
+        {
+            lock (syncObj)
+            {
+                //graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                DateTime start = DateTime.Now;
+
+                foreach (PointPair pointpair in GetPoints(width, height))
+                {
+                    Bitmap tex1d = null, onlyedges = null;
+
+                    try
+                    {
+                        tex1d = bmptex.Get1DTexture(pointpair.Start, pointpair.End);
+                        onlyedges = bmptex.ShowEdgePoints(tex1d, maxedges, Strategy, tolerance, edgecolor);
+
+                        ObjectSpaceRotateTransform(pointpair, angle); // rotate modelmatrix (obj-trans-mat * world-trans-mat)
+                        graphics.DrawImage(onlyedges, pointpair.Start);
+                        ObjectSpaceRotateTransform(pointpair, -angle); // -rotate viewmatrix (reset world transform matrix)
+                    }
+                    finally
+                    {
+                        if (tex1d != null)
+                        {
+                            tex1d.Dispose();
+                        }
+                        if (onlyedges != null)
+                        {
+                            onlyedges.Dispose();
+                        }
+                    }
+                }
+
+                return (DateTime.Now - start).TotalMilliseconds;
+            }
+        }
+
+        private void ObjectSpaceRotateTransform(PointPair axisPoint, int angle)
+        {
+            graphics.TranslateTransform(axisPoint.Start.X, axisPoint.Start.Y);
+            graphics.RotateTransform(angle);
+            graphics.TranslateTransform(-axisPoint.Start.X, -axisPoint.Start.Y);
+        }
+
+        private IList<PointPair> GetPoints(int width, int height)
+        {
+            IList<PointPair> result = new List<PointPair>();
+
+            double angle1 = Math.Tan((90 - angle) * DEGREE_TO_RADIAN);
+            double angle2 = Math.Tan(angle * DEGREE_TO_RADIAN);
+
+            int start = (int)(angle1 * height);
+            int end = width - start;
+
+            for (int i = 0; i < end; i++)
+            {
+                result.Add(new PointPair(new Point(i, 0), new Point(i + start, height)));
+            }
+
+            for (int i = 1; (i < height); i++)
+            {
+                result.Add(new PointPair(new Point(0, i), new Point((int)(angle1 * (height - i)), height)));
+            }
+
+            for (int i = end; i < width; i++)
+            {
+                result.Add(new PointPair(new Point(i, 0), new Point(width, (int)(angle2 * (width - i)))));
+            }
+
+            return result;
+        }
+    }
+}
